238f3ffb1f8089b42675d14e8189b8a3aa0203de
poorwym <wuy691128@gmail.com>
2026-07-07 14:34:36 +0800

    using vite-plugin-cesium

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import cesium from "vite-plugin-cesium";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
+    cesium(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
   server: {
